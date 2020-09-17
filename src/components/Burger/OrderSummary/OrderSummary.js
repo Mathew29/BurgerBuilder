d5fd4406e98859cb5bde99deab704f9b7480f75d
Mathew <mwakre29@gmail.com>
2020-09-17 14:52:03 -0700

    add style and functions to custom button

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Auxiliary from "../../../hoc/Auxiliary";
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -18,8 +19,8 @@ const orderSummary = (props) => {
       <p>A delicious burger with the following ingredient:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
-      <button>Cancel</button>
-      <button>Continue</button>
+      <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
+      <Button btnType="Success" clicked={props.purchaseContinue}>Continue</Button>
     </Auxiliary>
   );
 };
