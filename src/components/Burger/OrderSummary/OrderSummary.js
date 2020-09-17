b1c99a0dc3ccd6d33aa5e08f77dbecf0a318c945
Mathew <mwakre29@gmail.com>
2020-09-17 16:34:29 -0700

    change to class compnents

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Auxiliary from "../../../hoc/Auxiliary";
-import Button from '../../UI/Button/Button';
+import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
+    return (
+      <Auxiliary>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredient:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+          Cancel
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>
+          Continue
+        </Button>
+      </Auxiliary>
+    );
+  }
+}
 
-  return (
-    <Auxiliary>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredient:</p>
-      <ul>{ingredientSummary}</ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>Continue</Button>
-    </Auxiliary>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
