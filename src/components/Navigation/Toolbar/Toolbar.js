6d133335f038c62dd8f9813d60b7e8e240e5c60f
Mathew <mwakre29@gmail.com>
2020-09-17 15:43:22 -0700

    change inline to className

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import classes from './Toolbar.css'
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <Logo height="80%" />
-        <nav>
-            <NavigationItems />
-        </nav>
-    </header>
+  <header className={classes.Toolbar}>
+    <div>Menu</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default toolbar;
