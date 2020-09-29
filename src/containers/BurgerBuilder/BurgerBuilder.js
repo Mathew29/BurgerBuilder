693a287b1d7b2f66ee52040c4a1cec222525ccf6
Mathew <mwakre29@gmail.com>
2020-09-29 16:42:15 -0700

    refactor updatePurchaseState

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -40,7 +40,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 }); // sets true if sum is greater than 0
+    return sum > 0  // sets true if sum is greater than 0
   };
 
 
@@ -87,7 +87,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}
           />
